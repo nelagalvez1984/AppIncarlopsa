fa999db70a6311a39da2d8f0ebbb7bd774bc9dcc
Triscatib <32056556+Triscatib@users.noreply.github.com>
2017-10-25 12:14:00 +0200

    He rellenado la clase tipofichero y adjunto

--- app/src/main/java/incarlopsa/com/appincarlopsa/TipoFichero.java
+++ app/src/main/java/incarlopsa/com/appincarlopsa/TipoFichero.java
@@ -4,8 +4,37 @@ public class TipoFichero {
 
     //ToDO
 
-    //Id del tipo de fichero
-    //Nombre del tipo fichero
-    //Mostrable o no mostrable
+    private int id;
+    private String nombre;
+    private Boolean mostrable;
 
+    public TipoFichero(int id, String nombre, Boolean mostrable) {
+        this.id = id;
+        this.nombre = nombre;
+        this.mostrable = mostrable;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Boolean getMostrable() {
+        return mostrable;
+    }
+
+    public void setMostrable(Boolean mostrable) {
+        this.mostrable = mostrable;
+    }
 }
