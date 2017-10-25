1eae060e645de17c43ef3f469027062acc0555e8
Triscatib <32056556+Triscatib@users.noreply.github.com>
2017-10-25 12:51:13 +0200

    He rellenado la clase tipofichero y adjunto

--- app/src/main/java/incarlopsa/com/appincarlopsa/MensajeChat.java
+++ app/src/main/java/incarlopsa/com/appincarlopsa/MensajeChat.java
@@ -1,8 +1,20 @@
 package incarlopsa.com.appincarlopsa;
 
+import java.util.Date;
+
 public class MensajeChat extends MensajeBase {
 
+    private Boolean leidoPorDestino;
+
+    public MensajeChat(int idChat, int idMensaje, int idUsuario, String mensaje, Date fecha, Boolean leidoPorDestino) {
+        super(idChat, idMensaje, idUsuario, mensaje, fecha);
+        this.leidoPorDestino = leidoPorDestino;
+    }
+
+
     //ToDo
+
+
 
 
 }
--- app/src/main/java/incarlopsa/com/appincarlopsa/MensajeComentario.java
+++ app/src/main/java/incarlopsa/com/appincarlopsa/MensajeComentario.java
@@ -1,8 +1,18 @@
 package incarlopsa.com.appincarlopsa;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class MensajeComentario extends MensajeBase {
 
+    ArrayList<MeAlgo> arrayAlgo;
+
+    public MensajeComentario(int idPublicacion, int idMensaje, int idUsuario, String mensaje, Date fecha, ArrayList<MeAlgo> algos) {
+        super(idPublicacion, idMensaje, idUsuario, mensaje, fecha);
+        arrayAlgo = algos;
+    }
+
+
     //ToDo
-    //Contiene Array de Likes
 
 }
