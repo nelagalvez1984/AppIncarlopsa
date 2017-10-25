27734849ed30d847bc6d0a461a4329e2cca9f9f3
proyectoedge <proyectoedge@users.noreply.github.com>
2017-10-25 13:35:19 +0200

    Xml basico hecho en portrait y landscape - codigo en construccion

--- app/src/main/java/incarlopsa/com/appincarlopsa/VLogin.java
+++ app/src/main/java/incarlopsa/com/appincarlopsa/VLogin.java
@@ -1,8 +1,11 @@
 package incarlopsa.com.appincarlopsa;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class VLogin extends AppCompatActivity implements IVista{
 
@@ -12,12 +15,18 @@ public class VLogin extends AppCompatActivity implements IVista{
 
     @Override
     public void inicializarVista() {
-        //ToDO
+        //ToDo
     }
 
     @Override
     public void onClick(View v) {
-        //ToDO
+        Intent intent = new Intent(this, Conexion.class);
+        EditText etUsuario = (EditText)findViewById(R.id.etUsuario);
+        EditText etPassword = (EditText)findViewById(R.id.etPassword);
+
+
+
+
     }
 
 
@@ -25,6 +34,8 @@ public class VLogin extends AppCompatActivity implements IVista{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button btnconectar = (Button) findViewById(R.id.btnconectar);
+        btnconectar.setOnClickListener(this);
         inicializarVista();
     }
 }
