89c8f6d6a6dd1ba579c139c715830c586d9c9f2e
jonatanguca <jonatanguca@users.noreply.github.com>
2017-10-25 11:21:55 +0200

    1er commit jonatan

--- app/src/main/java/incarlopsa/com/appincarlopsa/Usuario.java
+++ app/src/main/java/incarlopsa/com/appincarlopsa/Usuario.java
@@ -1,7 +1,12 @@
 package incarlopsa.com.appincarlopsa;
 
 public class Usuario {
-
+    Integer idUsuario;
+    String nombre;
+    String apellidos;
+    String dni;
+    String tipoEmpleado;
+    Foto foto;
     //ToDo
 
     //Contiene una imagen de tipo Foto
