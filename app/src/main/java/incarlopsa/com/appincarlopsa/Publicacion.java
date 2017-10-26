4afd6cc6a5b59a42c960b2ba66b72f70b3e73673
Triscatib <32056556+Triscatib@users.noreply.github.com>
2017-10-26 10:18:23 +0200

    Chat, Publicacion y Topic con los cambios aconsejados

--- app/src/main/java/incarlopsa/com/appincarlopsa/Publicacion.java
+++ app/src/main/java/incarlopsa/com/appincarlopsa/Publicacion.java
@@ -1,11 +1,91 @@
 package incarlopsa.com.appincarlopsa;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+
+//Contiene un mensajeComentario con 0-X adjuntos
+//Contiene un arraylist de mensajeComentarios
+
+
 public class Publicacion extends Topic{
 
-    //ToDo
+    private int idPublicacion;
+    MensajeComentario mensajeComentario[];
+    ArrayList<MensajeComentario> mensajeComentarios;
+    ArrayList<Adjunto> adjuntos;
 
 
-    //Contiene un mensajeComentario con 0-X adjuntos
-    //Contiene un arraylist de mensajeComentarios
+    //Constructor
+    public Publicacion(String titulo, Date fecha, String autor, int idPublicacion, MensajeComentario[] mensajeComentario, ArrayList<MensajeComentario> mensajeComentarios, ArrayList<Adjunto> adjuntos) {
+        super(titulo, fecha, autor);
+        this.idPublicacion = idPublicacion;
+        this.mensajeComentario = mensajeComentario;
+        this.mensajeComentarios = mensajeComentarios;
+        this.adjuntos = adjuntos;
+    }
 
+
+    //Getters y setters
+    public int getIdPublicacion() {
+        return idPublicacion;
+    }
+
+    public void setIdPublicacion(int idPublicacion) {
+        this.idPublicacion = idPublicacion;
+    }
+
+    public MensajeComentario[] getMensajeComentario() {
+        return mensajeComentario;
+    }
+
+    public void setMensajeComentario(MensajeComentario[] mensajeComentario) {
+        this.mensajeComentario = mensajeComentario;
+    }
+
+    public ArrayList<MensajeComentario> getMensajeComentarios() {
+        return mensajeComentarios;
+    }
+
+    public void setMensajeComentarios(ArrayList<MensajeComentario> mensajeComentarios) {
+        this.mensajeComentarios = mensajeComentarios;
+    }
+
+    public ArrayList<Adjunto> getAdjuntos() {
+        return adjuntos;
+    }
+
+    public void setAdjuntos(ArrayList<Adjunto> adjuntos) {
+        this.adjuntos = adjuntos;
+    }
+
+    //Equals y hash
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Publicacion)) return false;
+        if (!super.equals(o)) return false;
+
+        Publicacion that = (Publicacion) o;
+
+        if (getIdPublicacion() != that.getIdPublicacion()) return false;
+        // Probably incorrect - comparing Object[] arrays with Arrays.equals
+        if (!Arrays.equals(getMensajeComentario(), that.getMensajeComentario())) return false;
+        if (getMensajeComentarios() != null ? !getMensajeComentarios().equals(that.getMensajeComentarios()) : that.getMensajeComentarios() != null)
+            return false;
+        return getAdjuntos() != null ? getAdjuntos().equals(that.getAdjuntos()) : that.getAdjuntos() == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + getIdPublicacion();
+        result = 31 * result + Arrays.hashCode(getMensajeComentario());
+        result = 31 * result + (getMensajeComentarios() != null ? getMensajeComentarios().hashCode() : 0);
+        result = 31 * result + (getAdjuntos() != null ? getAdjuntos().hashCode() : 0);
+        return result;
+    }
 }
+
+
--- app/src/main/java/incarlopsa/com/appincarlopsa/Topic.java
+++ app/src/main/java/incarlopsa/com/appincarlopsa/Topic.java
@@ -1,8 +1,9 @@
 package incarlopsa.com.appincarlopsa;
 
+import java.util.Date;
+
 public class Topic {
 
-    //ToDo
 
     //Es la cabecera de una publicacion o chat
 
@@ -11,6 +12,71 @@ public class Topic {
     //Fecha del ultimo update
     //Autor
 
+    //Propiedades
+    private String titulo;
+    private Date fecha;
+    private String Autor;
+    protected Boolean finalizado=false;
+
+    //Constructor
+    public Topic(String titulo, Date fecha, String autor) {
+        this.titulo = titulo;
+        this.fecha = fecha;
+        Autor = autor;
+    }
 
 
+    //Getter y Setter
+
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getAutor() {
+        return Autor;
+    }
+
+    public void setAutor(String autor) {
+        Autor = autor;
+    }
+
+
+    //Equals y hash
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Topic)) return false;
+
+        Topic topic = (Topic) o;
+
+        if (getTitulo() != null ? !getTitulo().equals(topic.getTitulo()) : topic.getTitulo() != null)
+            return false;
+        if (getFecha() != null ? !getFecha().equals(topic.getFecha()) : topic.getFecha() != null)
+            return false;
+        return getAutor() != null ? getAutor().equals(topic.getAutor()) : topic.getAutor() == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getTitulo() != null ? getTitulo().hashCode() : 0;
+        result = 31 * result + (getFecha() != null ? getFecha().hashCode() : 0);
+        result = 31 * result + (getAutor() != null ? getAutor().hashCode() : 0);
+        return result;
+    }
 }
