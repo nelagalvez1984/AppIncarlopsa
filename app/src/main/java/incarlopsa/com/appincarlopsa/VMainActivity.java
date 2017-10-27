01c0c5ed643c43291929e8745e5df6f7150f75b6
proyectoedge <proyectoedge@users.noreply.github.com>
2017-10-27 10:01:17 +0200

    hola soy edu

--- app/src/main/java/incarlopsa/com/appincarlopsa/VMainActivity.java
+++ app/src/main/java/incarlopsa/com/appincarlopsa/VMainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class VMainActivity extends AppCompatActivity implements IVista{
 
@@ -19,13 +20,17 @@ public class VMainActivity extends AppCompatActivity implements IVista{
 
     @Override
     public void onClick(View v) {
-        //ToDO
+        Intent intent=new Intent(this,VLogin.class);
+        startActivity(intent);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button=(Button)findViewById(R.id.button);
+        button.setOnClickListener(this);
+
         //ToDO
     }
 
