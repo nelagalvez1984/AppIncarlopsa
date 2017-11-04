46f59eaa37386900fe07b07faea3b1dec969000d
nelagalvez1984 <32056516+nelagalvez1984@users.noreply.github.com>
2017-11-04 17:57:57 +0100

    Mimando al grupo: Drawer hecho, a por todas!

---
+++ app/src/main/java/incarlopsa/com/appincarlopsa/DrawerLateral.java
@@ -0,0 +1,82 @@
+package incarlopsa.com.appincarlopsa;
+
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.view.MenuItem;
+
+/**
+ * Created by Nela on 04/11/2017.
+ */
+
+class DrawerLateral {
+
+    //Propiedades
+    private static final DrawerLateral ourInstance = new DrawerLateral();
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
+
+
+    static DrawerLateral getInstance() {
+        return ourInstance;
+    }
+
+    private DrawerLateral() {
+        navigationView = null;
+    }
+
+    public NavigationView getNavigationView() {
+        return navigationView;
+    }
+
+    public void setNavigationView(NavigationView navigationView) {
+        this.navigationView = navigationView;
+    }
+
+    public DrawerLayout getDrawerLayout() {
+        return drawerLayout;
+    }
+
+    public void setDrawerLayout(DrawerLayout drawerLayout) {
+        this.drawerLayout = drawerLayout;
+    }
+
+    private void setup() {
+        setup(navigationView);
+    }
+
+    private void setup(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        switch (menuItem.getItemId()) {
+                            case R.id.item_navigation_drawer_publicaciones: //Ver publicaciones
+                                menuItem.setChecked(true);
+                                //Hacer cosas
+                                drawerLayout.closeDrawer(GravityCompat.START);
+                                return true;
+                            case R.id.item_navigation_drawer_chats: //Ver chats
+                                menuItem.setChecked(true);
+                                //Hacer cosas
+                                drawerLayout.closeDrawer(GravityCompat.START);
+                                return true;
+                            case R.id.item_navigation_drawer_nuevo_chat: //Crear chat
+                                menuItem.setChecked(true);
+                                //Hacer cosas
+                                drawerLayout.closeDrawer(GravityCompat.START);
+                                return true;
+                            case R.id.item_navigation_drawer_nueva_publicacion: //Crear publicacion
+                                menuItem.setChecked(true);
+                                //Hacer cosas
+                                drawerLayout.closeDrawer(GravityCompat.START);
+
+                                return true;
+                        }
+                        return true;
+                    }
+                });
+    }
+
+}
--- app/src/main/java/incarlopsa/com/appincarlopsa/VMainActivity.java
+++ app/src/main/java/incarlopsa/com/appincarlopsa/VMainActivity.java
@@ -1,85 +1,143 @@
 package incarlopsa.com.appincarlopsa;
 
 import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.Toolbar;
-
-import java.util.ArrayList;
-
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class VMainActivity extends AppCompatActivity implements IVista, ICodigos {
 
-    // propiedad para la barra de herramientas
-    // propiedad para la barra de herramientas
-    private Toolbar toolbar;
-
     //Propiedades
-    SingleCredenciales credenciales = SingleCredenciales.getInstance();
-    TESTFactoriaItems factoriaItems = new TESTFactoriaItems();
-    ArrayList<DataBaseItem> resultados;
-    Button boton1;
-    Button boton2;
-    Button boton3;
-    Button boton4;
-    Button boton5;
-    Button boton6;
-    Button boton7;
-    Button boton8;
-    Integer tamano;
-    Integer contador = 0;
-    Boolean retornoVerdaderoOFalso = false;
+    private SingleCredenciales credenciales = SingleCredenciales.getInstance();
+    private TESTFactoriaItems factoriaItems = new TESTFactoriaItems();
 
-
-    @Override
-    public void inicializarVista() {
-        credenciales.setLogin(USUARIO_TEST_NORMAL);
-        credenciales.setPassword(PASSWORD_TEST_NORMAL);
-        boton1 = (Button) findViewById(R.id.btnTest1);
-        boton2 = (Button) findViewById(R.id.btnTest2);
-        boton3 = (Button) findViewById(R.id.btnTest3);
-        boton4 = (Button) findViewById(R.id.btnTest4);
-        boton5 = (Button) findViewById(R.id.btnTest5);
-        boton6 = (Button) findViewById(R.id.btnTest6);
-        boton7 = (Button) findViewById(R.id.btnTest7);
-        boton8 = (Button) findViewById(R.id.btnTest8);
-
-        boton8.setOnClickListener(this);
-
-    }
-
-    @Override
-    public void onClick(View v) {
-/*
-        tamano = resultados.size();
-        Usuario temp = (Usuario)resultados.get(contador);
-        contador++;
-        contador %= tamano;
-
-        //PARA READ
-        boton1.setText(temp.getIdUsuario().toString());
-        boton2.setText(temp.getNombre());
-        boton3.setText(temp.getApellidos());
-        boton4.setText(temp.getDni());
-        boton5.setText(temp.getTipoEmpleado());
-        boton6.setText(temp.getFoto().toString());
- */
-        //PARA CREATE
-        boton1.setText(retornoVerdaderoOFalso.toString());
-        //boton7.setText(temp.getUsername());
-
-        //ToDO
-    }
+    private DrawerLayout drawerLayout;
+    private Toolbar toolbar;
+    private ActionBar actionBar;
+    private ImageView fondo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-/*
+
+
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        if (navigationView != null) {
+            setupDrawer(navigationView);
+        }
+
+        setupDrawer(navigationView);
+
+        inicializarVista();
+
+    }
+
+    private void setupDrawer(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        switch (menuItem.getItemId()) {
+                            case R.id.item_navigation_drawer_publicaciones: //Ver publicaciones
+                                menuItem.setChecked(true);
+                                //Hacer cosas
+                                drawerLayout.closeDrawer(GravityCompat.START);
+                                return true;
+                            case R.id.item_navigation_drawer_chats: //Ver chats
+                                menuItem.setChecked(true);
+                                //Hacer cosas
+                                drawerLayout.closeDrawer(GravityCompat.START);
+                                return true;
+                            case R.id.item_navigation_drawer_nuevo_chat: //Crear chat
+                                menuItem.setChecked(true);
+                                //Hacer cosas
+                                drawerLayout.closeDrawer(GravityCompat.START);
+                                return true;
+                            case R.id.item_navigation_drawer_nueva_publicacion: //Crear publicacion
+                                menuItem.setChecked(true);
+                                //Hacer cosas
+                                drawerLayout.closeDrawer(GravityCompat.START);
+                                Intent intent = new Intent(VMainActivity.this, SettingsActivity.class);
+                                startActivity(intent);
+                                return true;
+                        }
+                        return true;
+                    }
+                });
+    }
+
+    @Override
+    public void inicializarVista() {
+
+        credenciales.setLogin(USUARIO_TEST_NORMAL);
+        credenciales.setPassword(PASSWORD_TEST_NORMAL);
+
+        //Fondo
+        fondo = (ImageView)findViewById(R.id.fondoInicial);
+        fondo.setOnClickListener(this);
+
+        //Sustituir toolbar
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //ActionBar
+        actionBar = getSupportActionBar();
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+
+        //Fijar el drawerLayout
+        drawerLayout = (DrawerLayout) findViewById(R.id.navigation_drawer_layout);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+    @Override
+    public void onClick(View v) {
+        //Al hacer click en el fondo, sacar el menu
+        switch (v.getId()){
+            case R.id.fondoInicial:
+                drawerLayout.openDrawer(GravityCompat.START);
+                break;
+        }
+
+
+    }
+
+    private void tests(){
+        /*
         // llamamos al toolbar creado
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -173,11 +231,7 @@ public class VMainActivity extends AppCompatActivity implements IVista, ICodigos
         resultados = factoriaItems.testReadGenerico(DAME_LOS_TOPIC , new DAOPublicacion());
         */
 
-        SingleCredenciales singleCredenciales = SingleCredenciales.getInstance();
-        singleCredenciales.setIdUsuario(2);
-        singleCredenciales.setLogin(USUARIO_TEST_NORMAL);
-        singleCredenciales.setPassword(PASSWORD_TEST_NORMAL);
-/*
+        /*
 /*
         resultados = factoriaItems.testReadGenerico(DAME_LOS_TOPIC_HACIA_MI, new DAOChat());
 */
@@ -207,11 +261,12 @@ public class VMainActivity extends AppCompatActivity implements IVista, ICodigos
 
         retornoVerdaderoOFalso = factoriaItems.testUpdateGenerico(m, new DAOMensaje());
 */
+/*
 
         Chat c = new Chat();
         c.setId(5);
         retornoVerdaderoOFalso = factoriaItems.testDeleteGenerico(c , new DAOChat());
-
+*/
 /*
         resultados = factoriaItems.testReadGenerico(c , new DAOChat());
 
@@ -219,40 +274,6 @@ public class VMainActivity extends AppCompatActivity implements IVista, ICodigos
         c.setTitulo("Lo he cambiado!");
         retornoVerdaderoOFalso = factoriaItems.testUpdateGenerico(c, new DAOChat());
 */
-        System.out.print("QUIETOOOL");
-        System.out.print("QUIETOOOL-2");
-        //ToDO
-    }
-
-    private void setSupportActionBar(Toolbar toolbar) {
-    }
-
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        //ToDO
-    }
-
-    // éste método sirve para crear las opciones del menú
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu, menu);
-        return true;
-    }
-
-    //
-    @Override
-    public boolean onOptionsItemSelected(MenuItem opcionMenu) {
-        int id = opcionMenu.getItemId();
-
-        if (id == R.id.mensaje) {
-            return true;
-        }
-
-        if (id == R.id.chat) {
-            return true;
-        }
-        return super.onOptionsItemSelected(opcionMenu);
     }
 
 }
