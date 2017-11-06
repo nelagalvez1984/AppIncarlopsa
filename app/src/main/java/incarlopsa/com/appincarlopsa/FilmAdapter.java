14f5be4722aae3686724cf11e390ff876ea3c16e
seatclk <seatclk@users.noreply.github.com>
2017-11-06 11:39:57 +0100

    commit

--- app/src/main/java/incarlopsa/com/appincarlopsa/FilmAdapter.java
+++ app/src/main/java/incarlopsa/com/appincarlopsa/FilmAdapter.java
@@ -1,15 +1,25 @@
 package incarlopsa.com.appincarlopsa;
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
 
 /**
- * Created by David on 06/11/2017.
+ * Created by malozano on 25/12/16.
  */
 
-public class MensajeAdapter {
-    private Chat<Mensaje> mMensajes;
+public class FilmAdapter extends RecyclerView.Adapter<FilmAdapter.ViewHolder> {
 
+    private List<Film> mFilms;
 
-    public MensajeAdapter(Chat<Mensaje> mensajes) {
-        mMensajes = mensajes;
+    public FilmAdapter(List<Film> films) {
+        mFilms = films;
     }
 
     @Override
@@ -23,7 +33,7 @@ public class MensajeAdapter {
             public void onClick(View v) {
                 int position = holder.getAdapterPosition();
                 if(mListener!=null) {
-                    mListener.onItemClick(mMensajes.get(position), position);
+                    mListener.onItemClick(mFilms.get(position), position);
                 }
             }
         });
@@ -33,12 +43,12 @@ public class MensajeAdapter {
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        holder.bind(mMensajes.get(position));
+        holder.bind(mFilms.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return mMensajes.size();
+        return mFilms.size();
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
@@ -70,5 +80,4 @@ public class MensajeAdapter {
     public void setOnItemListener(OnItemClickListener listener) {
         mListener = listener;
     }
-}
 }
