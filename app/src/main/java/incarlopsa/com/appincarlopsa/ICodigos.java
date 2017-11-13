8d2ce66c3f45f90871dfde6e46f1949d0ce5dd42
nelagalvez1984 <32056516+nelagalvez1984@users.noreply.github.com>
2017-11-13 09:21:13 +0100

    APP completa.
    - Acicalado de codigo:
    ----> Faltan Vistas
    A) VCs (Cabeceras, chat y creares)
    B) VMostrar
    C) VPublicacion
    ----> Faltan @strings
    
    - Testeo final
    - JavaDocs

--- app/src/main/java/incarlopsa/com/appincarlopsa/ICodigos.java
+++ app/src/main/java/incarlopsa/com/appincarlopsa/ICodigos.java
@@ -18,8 +18,8 @@ public interface ICodigos {
     String DAME_LOS_TOPIC_HACIA_MI = "DAME_LOS_TOPIC_HACIA_MI";
 
     //Codigos para conexion a la BBDD
-    String SERVIDOR_BBDD = "192.168.0.101";
-//    String SERVIDOR_BBDD = "10.20.32.116";
+//    String SERVIDOR_BBDD = "192.168.0.101";
+    String SERVIDOR_BBDD = "10.20.32.116";
     String PUERTO_BBDD = "3306";
     String BBDD = "incarlopsa";
     String DIRECCION_BBDD_RAIZ = "jdbc:mysql://"+SERVIDOR_BBDD + ":" + PUERTO_BBDD
